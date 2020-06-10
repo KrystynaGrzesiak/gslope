cf0eb2bc9b4520c1697a85c3d7961a9386155215
KrystynaGrzesiak <krygrz11@gmail.com>
2020-06-10 20:05:14 +0200

    adds more plots

--- gslope/R/plot.R
+++ gslope/R/plot.R
@@ -1,36 +1,75 @@
 #' @importFrom ggplot2 "ggplot"
 #' @importFrom ggplot2 "geom_tile"
 #' @importFrom ggplot2 "scale_fill_gradient"
+#' @importFrom ggplot2 "aes"
+#' @importFrom ggplot2 "ggtitle"
+#' @importFrom ggplot2 "xlab"
+#' @importFrom ggplot2 "ylab"
+#' @importFrom ggplot2 "theme"
+#' @importFrom ggplot2 "labs"
+#' @importFrom ggplot2 "element_text"
+#' @importFrom GGally "ggcorr"
 
 #' @title Plot precision matrix
 #'
 #' @param x an object of class `'gslope'`
-#' @param col a character, plot color. Default "black".
+#' @param col a character, color name. Default "black"
+#' @param plt a plot type. Accepts either \code{'precision'}, \code{'corr'} or \code{'scaled_precision'}. Default \code{'precision'}.
 #'
 #' @return Prints output on the screen
 #'
 #' @examples
-#' bla bla
-#' @rdname plot
+#' w = gslope(as.matrix(mtcars))
+#' plot(w, col = "green", plt = "precision")
+#' plot(w, plt = "corr")
+#' plot(w,  col = "purple", plt = "scaled_precision")
+
 #' @export
 
 plot <- function(x, col){
   UseMethod('plot')
 }
 
-#'
+#' @rdname plot
 #' @export
 #'
-plot.gslope = function(x, col="black"){
-  precision_matrix = x$precision_matrix
-  x = colnames(precision_matrix)
-  y = x
-  data = expand.grid(X=x, Y=y)
-  data$Z = c(precision_matrix)
+plot = function(x, plt = "precision", col = "black"){
+  
+  if(!(plt %in% c("precision", "corr", "scaled_precision"))) stop("Plt must be either precision, corr or scaled_precision")
 
-  ggplot(data, mapping = aes(X, Y, fill= Z)) + 
-    geom_tile() +
-    scale_fill_gradient(low="white", high=col)
+  if(plt == "precision") {
+    print("halo")
+    precision_matrix = x$precision_matrix
+    x = colnames(precision_matrix)
+    y = x
+    data = expand.grid(X = x, Y = y)
+    data$value = c(precision_matrix)
+    
+    result = ggplot(data, mapping = aes(X, Y, fill = value)) + 
+      geom_tile() +
+      scale_fill_gradient(low = "white", high = col) +
+      ggtitle("Precision matrix") +
+      ylab("") +
+      xlab("") +
+      theme(plot.title = element_text(hjust = 0.5))
+  }
+  if(plt == "corr") {
+    result = ggcorr(cov2cor(result$covariance_matrix), 
+           nbreaks = 5, 
+           low = col, 
+           high = col)+
+      labs(title = "Correlation matrix") +
+      theme(plot.title = element_text(hjust = 0.5))
+  }
+  if(plt == "scaled_precision") {
+    result = ggcorr(result$scaled_precision_matrix, 
+           nbreaks = 5, 
+           low = col, 
+           high = col)+
+      labs(title = "Scaled precision matrix") +
+      theme(plot.title = element_text(hjust = 0.5))
+  }
+  result
 }
 
 

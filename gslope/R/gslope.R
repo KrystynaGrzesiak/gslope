36d730dccab1155f64c812425ca0c0842d046d8b
Zosia <zosiadziedzic.33@gmail.com>
2020-06-16 19:27:02 +0200

    fixes names for data without colnames

--- gslope/R/gslope.R
+++ gslope/R/gslope.R
@@ -34,31 +34,38 @@ prepare_lambda = function(lambda, low_tri_size) {
 
 #' @title Graphical SLOPE
 #' @description  Computes precision matrix for graph models using graphical SLOPE.
+#'
 #' @param data a matrix containing observations of variables of interest.
 #' @param lambda vector of regularizers for SLOPE. By default computed based on Benjamini-Hochberg's method.
 #' @param sample_cov variance-covariance matrix.
-#' @param scaled logical. The data need to be scaled so that it has mean = 0 and variance = 1. If TRUE, build-in data scaling will be omitted.
-#' @param mu ## TODO:  czo to?
+#' @param scaled {logical. The data need to be scaled so that it has mean = 0 and variance = 1. If TRUE, build-in data scaling will be omitted.}
+#' @param mu correction for lambda scaling in ADMM algorithm.
 #' @param max_iter maximum number of iterations allowed in ADMM algorithm. Default 10 000.
-#' @param threshold a value used to determine which variables in graphical model are not linked by an edge. The larger the value, the more variables will be unlinked. Default 10e-4.
+#' @param threshold {a value used to determine which variables in graphical model are not linked by an edge. The larger the value, the more variables will be unlinked. Default 10e-4.}
 #' @param epsilon a value used to determine accuracy of the ADMM algorithm. Default 10e-4.
 #' @param alpha significance level.
+#'
 #' @keywords precision matrix, graphical slope
-#' @return \code{gslope} returns a list containing following components:
-#' \item{precision_matrix}{a precision matrix revealing graph structure for the data.}
-#' \item{covariance_matrix}{covariance matrix equal to the inverse of the presicion matrix.}
-#' \item{scaled_precision_matrix}{...}
-#' \item{lambda}{a vector of penalty parameters used in SLOPE.}
-#' \item{iterations}{a number of iterations performed in ADMM algorithm.}
-#' \item{graph}{a graph based on precision matrix.}
-#' \item{clusters}{aconnected entries of a graph.}
+#'
+#' @return {returns a list containing following components:
+#' \itemize{
+#' \item \code{precision_matrix} a precision matrix revealing graph structure for the data.
+#' \item \code{covariance_matrix} covariance matrix equal to the inverse of the presicion matrix.
+#' \item \code{scaled_precision_matrix} {An element with coordinates (k,l) is given by formula:\cr
+#' k_lm=-Cov(X_l,X_m|X_(V\ \{l,m\}))/(Var(X_l|X_(V\ \{l,m\}))^(1/2)*Var(X_m|X_(V\ \{l,m\}))^(1/2)).}
+#' \item \code{lambda} a vector of penalty parameters used in SLOPE.
+#' \item \code{iterations} a number of iterations performed in ADMM algorithm.
+#' \item \code{graph} an IGRAPH object returning set of edges between vertices in the model. For more details see ?igraph
+#' \item \code{clusters} {a sub-graph structure. Numbers associeted with vertex indicates which sub-graph the vertex belongs to.
+#' If all verices are marked with ones, then there is only one graph in the model. Size an number of clusters are additionaly
+#' provided in separate vectors. For more details see ?clusters.}
+#' }}
+#'
 #' @details \code{gslope} selects high probability graph structure for graphical model with likelihood-based methods combined with ordered L1-regularization. Namely, it solves - using ADMM algorithm - the following  maximization problem:
 #' \deqn{ log det \Theta - tr(S \Theta) - \lambda(\Theta), subject to \Theta \in S+,}
 #' where S is a sample covariance matrix, \eqn{\lambda(\Theta)} is a series of regularizers for SLOPE and S+ denotes a set of symmetric, semidefinite matrices.
 #' @examples
 #' gslope(mtcars, epsilon = 1e-3)
-## TODO: jakis przykladzik
-## TODO: co to jest scaled_precision matrix?????
 #' @references Makowski, M., (2018).
 #' Precision matrix estimation in Gaussian graphical models.
 #' Master's Thesis. Uniwersytet Wroclawski.

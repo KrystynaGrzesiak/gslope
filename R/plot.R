52f6996c7835b95498045257b44884c262001dfe
KrystynaGrzesiak <krygrz11@gmail.com>
2020-08-28 15:29:29 +0200

    replaces GGally with ggplot2 in case of correlation plot

--- R/plot.R
+++ R/plot.R
@@ -1,6 +1,8 @@
 #' @importFrom ggplot2 "ggplot"
 #' @importFrom ggplot2 "geom_tile"
 #' @importFrom ggplot2 "scale_fill_gradient"
+#' @importFrom ggplot2 "scale_fill_gradient2"
+#' @importFrom ggplot2 "coord_fixed"
 #' @importFrom ggplot2 "aes"
 #' @importFrom ggplot2 "ggtitle"
 #' @importFrom ggplot2 "xlab"
@@ -34,7 +36,7 @@ plot <- function(x, plt, col){
 #' @export
 #'
 plot.gslope = function(x, plt = "scaled_precision", col = "black"){
-  
+
   if(!(plt %in% c("precision", "corr", "scaled_precision", "covariance"))) stop("Plt must be either precision, covariance, corr or scaled_precision")
 
   if(plt == "scaled_precision") {
@@ -43,8 +45,8 @@ plot.gslope = function(x, plt = "scaled_precision", col = "black"){
     y = X
     data = expand.grid(X = X, Y = y)
     data$value = c(scaled_precision)
-    
-    result = ggplot(data, mapping = aes(X, Y, fill = value)) + 
+
+    result = ggplot(data, mapping = aes(X, Y, fill = value)) +
       geom_tile() +
       scale_fill_gradient(low = col, high = "white") +
       ggtitle("Scaled precision matrix") +
@@ -52,15 +54,15 @@ plot.gslope = function(x, plt = "scaled_precision", col = "black"){
       xlab("") +
       theme(plot.title = element_text(hjust = 0.5))
   }
-  
+
   if(plt == "precision") {
     precision_matrix = x$precision_matrix
     X = colnames(precision_matrix)
     y = X
     data = expand.grid(X = X, Y = y)
     data$value = c(precision_matrix)
-    
-    result = ggplot(data, mapping = aes(X, Y, fill = value)) + 
+
+    result = ggplot(data, mapping = aes(X, Y, fill = value)) +
       geom_tile() +
       scale_fill_gradient(low = "white", high = col) +
       ggtitle("Precision matrix") +
@@ -69,22 +71,34 @@ plot.gslope = function(x, plt = "scaled_precision", col = "black"){
       theme(plot.title = element_text(hjust = 0.5))
   }
   if(plt == "corr") {
-    result = ggcorr(cov2cor(x$covariance_matrix), 
-           nbreaks = 5, 
-           low = col, 
-           high = col)+
-      labs(title = "Correlation matrix") +
-      theme(plot.title = element_text(hjust = 0.5))
+    corr_matrix = cov2cor(x$covariance_matrix)
+    corr_matrix[upper.tri(corr_matrix)] = NA
+    X = colnames(corr_matrix)
+    y = X
+    data = expand.grid(X = X, Y = y)
+    data$value = c(corr_matrix)
+    data = data[!is.na(data[["value"]]), ]
+
+    result = ggplot(data, aes(X, Y, fill = value))+
+      geom_tile(color = "white")+
+      scale_fill_gradient2(low = "blue", high = "red", mid = "white",
+                           midpoint = 0, limit = c(-1,1), space = "Lab",
+                           name="Correlation") +
+      theme(axis.text.x = element_text(angle = 45, vjust = 1,
+                                       size = 10, hjust = 1))+
+      ylab("") +
+      xlab("") +
+      coord_fixed()
   }
-  
+
   if(plt == "covariance") {
     covariance = x$covariance_matrix
     X = colnames(covariance)
     y = X
     data = expand.grid(X = X, Y = y)
     data$value = c(covariance)
-    
-    result = ggplot(data, mapping = aes(X, Y, fill = value)) + 
+
+    result = ggplot(data, mapping = aes(X, Y, fill = value)) +
       geom_tile() +
       scale_fill_gradient(low = "white", high = col) +
       ggtitle("Covariance matrix") +
